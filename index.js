af545feb8856537024d1d1eba98d3f7111606d4b
AviadDerli <Strading.ea@gmail.com>
2023-08-20 21:13:27 +0300

    change port variable

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 const express = require("express");
 const cors = require('cors');
 const app = express();
-const PORT = 6565;
+const PORT = process.env.PORT || 6565;
 
 app.use(express.json());
 app.use(cors());
